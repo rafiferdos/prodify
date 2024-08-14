17b96e3504c5bf5ed0a991425aada9dea7e7c282
Rafi Ferdos <rafiferdos@gmail.com>
2024-08-14 22:20:37 +0600

    chore: Add authentication provider and router to main.jsx

---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,108 @@
+/* eslint-disable react/prop-types */
+import { createContext, useEffect, useState } from 'react'
+import {
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword,
+    getAuth,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    signOut,
+    updateProfile,
+} from 'firebase/auth'
+import { app } from '../firebase/firebase.config'
+import axios from 'axios'
+
+export const AuthContext = createContext(null)
+const auth = getAuth(app)
+const googleProvider = new GoogleAuthProvider()
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const signIn = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const signInWithGoogle = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    const logOut = async () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+
+    const updateUserProfile = (name, photo) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name,
+            photoURL: photo,
+        })
+    }
+
+    const getToken = async email => {
+        const { data } = await axios.post(
+            `${import.meta.env.VITE_API_URL}/jwt`,
+            { email },
+            { withCredentials: true }
+        )
+        return data
+    }
+
+    const saveUser = async user => {
+        const currentUser = {
+            name: user?.displayName,
+            image: user?.photoURL,
+            email: user?.email,
+            role: 'user',
+            status: 'Verified',
+        }
+        const { data } = await axios.put(
+            `${import.meta.env.VITE_API_URL}/save-user`,
+            currentUser
+        )
+        return data
+    }
+
+
+    // onAuthStateChange
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            setLoading(false)
+            if (currentUser) {
+                getToken(currentUser.email)
+                saveUser(currentUser)
+            }
+        })
+        return () => {
+            return unsubscribe()
+        }
+    }, [])
+
+    const authInfo = {
+        user,
+        setUser,
+        loading,
+        setLoading,
+        createUser,
+        signIn,
+        signInWithGoogle,
+        logOut,
+        updateUserProfile,
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+    )
+}
+
+export default AuthProvider
