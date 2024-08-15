30ade89047c2eeb3f2a93e71c679a253411a0282
Rafi Ferdos <rafiferdos@gmail.com>
2024-08-15 17:51:31 +0600

    chore: Update dependencies in package.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes/Routes'
-import AuthProvider from './provider/AuthProvider'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import AuthProvider from "./provider/AuthProvider";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <AuthProvider>
       <RouterProvider router={router} />
     </AuthProvider>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
